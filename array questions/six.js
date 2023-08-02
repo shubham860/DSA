2eaeff348f27cbf5b86979db12dbdd820efbfacf
Shubham Chauhan <Shubham.Chauhan860@gmail.com>
2023-08-03 01:34:13 +0530

    fix(array): maxProduct is solved

---
+++ array questions/six.js
@@ -0,0 +1,22 @@
+// Find Maximum value of abs(i – j) * min(arr[i], arr[j]) in an array arr[]
+
+function maxAbsoluteDifference(array) {
+    let maxProduct = 0;
+    let currentProduct = 0;
+    let i=0; j=array.length-1;
+    while(i<j) {
+        if(array[i] < array[j]) {
+            currentProduct = array[i] * Math.abs(i-j);
+            i++;
+        }else{
+            currentProduct = array[j] * Math.abs(i-j);
+            j--;
+        }
+        maxProduct = Math.max(maxProduct, currentProduct);
+    }   
+    return maxProduct;
+}
+ 
+console.log(maxAbsoluteDifference([8, 1, 9, 4])); // Expected Output: 16
+console.log(maxAbsoluteDifference([3, 2, 1, 4])); // Expected Output: 6
+console.log(maxAbsoluteDifference([5, 5, 5, 5])); // Expected Output: 15
